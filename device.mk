3cc2897f7ce342b63f3cb2017f7eb2de08eed1e7
LuK1337 <priv.luk@gmail.com>
2020-07-01 13:18:14 +0200

    OP4A79: Add product privapp permissions

--- device.mk
+++ device.mk
@@ -92,3 +92,6 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_BOOT_JARS += \
     telephony-ext
+
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/privapp-permissions-qti.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/privapp-permissions-qti.xml
