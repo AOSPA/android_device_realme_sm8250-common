db58ad506eeea63a85fe3d5706ee6410bb2b2542
Isaac Chen <tingyi364@gmail.com>
2020-08-29 22:20:09 +0800

    OP4A79: Add WiFi display blob
    Signed-off-by: Isaac Chen <tingyi364@gmail.com>

--- device.mk
+++ device.mk
@@ -138,3 +138,13 @@ PRODUCT_BOOT_JARS += \
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/privapp-permissions-qti.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/privapp-permissions-qti.xml
+
+# WiFi Display
+PRODUCT_PACKAGES += \
+    libnl
+
+PRODUCT_BOOT_JARS += \
+    WfdCommon
+
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/privapp-permissions-wfd.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-wfd.xml
