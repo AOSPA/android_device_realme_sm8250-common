bbaa0b4b9fe4da2ef7cf2781253234fcd3f23163
LuK1337 <priv.luk@gmail.com>
2020-09-18 13:12:09 +0200

    OP4A79: Drop android.hardware.thermal HAL
    android.hardware.thermal@1.0::IThermal/default is deprecated in compatibility matrix at FCM Version 4; it should not be served.
    
    Change-Id: Icc3c672a0bf6b9e4ee76ccf91e6cdca5fb778fde

--- device.mk
+++ device.mk
@@ -395,12 +395,6 @@ PRODUCT_BOOT_JARS += \
 PRODUCT_PACKAGES += \
     textclassifier.bundle1
 
-# Thermal
-PRODUCT_PACKAGES += \
-    android.hardware.thermal@1.0-impl \
-    android.hardware.thermal@1.0-service \
-    thermal.kona
-
 # USB
 PRODUCT_PACKAGES += \
     android.hardware.usb@1.0-service
