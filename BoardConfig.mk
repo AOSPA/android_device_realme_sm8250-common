2c0d3fca62f56865611e7c2a907d643bcf59518e
Alessandro Astone <ales.astone@gmail.com>
2020-11-03 19:26:18 +0100

    bitra: Remove serial console from cmdline

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -93,7 +93,7 @@ BOARD_KERNEL_PAGESIZE := 4096
 BOARD_BOOT_HEADER_VERSION := 2
 BOARD_MKBOOTIMG_ARGS += --header_version $(BOARD_BOOT_HEADER_VERSION)
 
-BOARD_KERNEL_CMDLINE := console=ttyMSM0,115200n8 earlycon=msm_geni_serial,0xa90000 androidboot.hardware=qcom androidboot.console=ttyMSM0 androidboot.memcg=1 lpm_levels.sleep_disabled=1 video=vfb:640x400,bpp=32,memsize=3072000 msm_rtb.filter=0x237 service_locator.enable=1 androidboot.usbcontroller=a600000.dwc3 swiotlb=2048 loop.max_part=7 cgroup.memory=nokmem,nosocket reboot=panic_warm kpti=off
+BOARD_KERNEL_CMDLINE := earlycon=msm_geni_serial,0xa90000 androidboot.hardware=qcom androidboot.memcg=1 lpm_levels.sleep_disabled=1 video=vfb:640x400,bpp=32,memsize=3072000 msm_rtb.filter=0x237 service_locator.enable=1 androidboot.usbcontroller=a600000.dwc3 swiotlb=2048 loop.max_part=7 cgroup.memory=nokmem,nosocket reboot=panic_warm kpti=off
 
 BOARD_KERNEL_IMAGE_NAME := Image
 TARGET_FORCE_PREBUILT_KERNEL := true
