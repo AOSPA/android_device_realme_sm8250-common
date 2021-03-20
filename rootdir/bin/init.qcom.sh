4d589b831bd4b5d3272e998ef1c652c2a9610568
Isaac Chen <tingyi364@gmail.com>
2021-03-20 17:48:21 +0800

    OP4A79: Update rootdir from stock
    Signed-off-by: Isaac Chen <tingyi364@gmail.com>

--- rootdir/bin/init.qcom.sh
+++ rootdir/bin/init.qcom.sh
@@ -456,12 +456,12 @@ buildvariant=`getprop ro.build.type`
 case "$buildvariant" in
     "userdebug" | "eng")
         #set default loglevel to KERN_INFO
-        #if VENDOR_EDIT
-        #Canjie.Zheng@PSW.AD.OppoDebug.LogKit.1078692, 2017/11/20, Add for modified kernel log level
+        #ifdef OPLUS_FEATURE_LOGKIT
+        #Canjie.Zheng@ANDROID.DEBUG.1078692, 2017/11/20, Add for modified kernel log level
         echo "1 6 1 7" > /proc/sys/kernel/printk
         #else
         #echo "6 6 1 7" > /proc/sys/kernel/printk
-        #endif
+        #endif OPLUS_FEATURE_LOGKIT
         ;;
     *)
         #set default loglevel to KERN_WARNING
