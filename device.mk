28f32f28486122c4aba0c74fa301e267ed33c0c0
Isaac Chen <tingyi364@gmail.com>
2021-07-26 16:47:18 +0200

    OP4A79: Build display stack from source
    Signed-off-by: Isaac Chen <tingyi364@gmail.com>

--- device.mk
+++ device.mk
@@ -178,14 +178,28 @@ PRODUCT_PACKAGES += \
 
 # Display
 PRODUCT_PACKAGES += \
+    android.hardware.graphics.mapper@3.0-impl-qti-display \
+    android.hardware.graphics.mapper@4.0-impl-qti-display \
     android.hardware.memtrack@1.0-impl \
     android.hardware.memtrack@1.0-service \
+    gralloc.kona \
+    libdisplayconfig.qti \
+    libdisplayconfig.qti.vendor \
+    libqdMetaData \
+    libsdmcore \
+    libsdmutils \
     libtinyxml \
     libvulkan \
+    memtrack.kona \
     vendor.display.config@1.16.vendor \
     vendor.display.config@2.0.vendor \
-    vendor.qti.hardware.display.allocator@3.0 \
-    vendor.qti.hardware.display.mapper@3.0.vendor
+    vendor.qti.hardware.display.allocator-service \
+    vendor.qti.hardware.display.composer-service \
+    vendor.qti.hardware.display.mapper@1.0.vendor \
+    vendor.qti.hardware.display.mapper@1.1.vendor \
+    vendor.qti.hardware.display.mapper@2.0.vendor \
+    vendor.qti.hardware.display.mapper@3.0.vendor \
+    vendor.qti.hardware.display.mapper@4.0.vendor
 
 # Doze
 PRODUCT_PACKAGES += \
