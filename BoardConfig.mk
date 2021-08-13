03f0a834c535a13f73efc99639b8d299193fe5a0
Isaac Chen <tingyi364@gmail.com>
2021-08-13 09:39:26 +0200

    OP4A79: Switch to rebased kernel
    Signed-off-by: Isaac Chen <tingyi364@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -121,7 +121,7 @@ TARGET_KERNEL_ADDITIONAL_FLAGS := DTC_EXT=$(shell pwd)/prebuilts/misc/$(HOST_OS)
 TARGET_KERNEL_ARCH := arm64
 TARGET_KERNEL_CLANG_COMPILE := true
 TARGET_KERNEL_SOURCE := kernel/oppo/sm8250
-TARGET_KERNEL_CONFIG := vendor/kona-perf_defconfig
+TARGET_KERNEL_CONFIG := op4a79_defconfig
 
 # Platform
 TARGET_BOARD_PLATFORM_GPU := qcom-adreno650
