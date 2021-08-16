51f7cf9d3538b9764f4ab63e701cc62232fabe9c
Isaac Chen <tingyi364@gmail.com>
2021-08-16 06:03:16 +0200

    OP4A79: sepolicy: Label some files/devices
     * Also remove unused labels
    
    Signed-off-by: Isaac Chen <tingyi364@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -187,7 +187,6 @@ VENDOR_SECURITY_PATCH := 2021-08-05
 # SELinux
 include device/qcom/sepolicy_vndr/SEPolicy.mk
 
-BOARD_PLAT_PRIVATE_SEPOLICY_DIR += $(DEVICE_PATH)/sepolicy/private
 BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
 
 # Telephony
