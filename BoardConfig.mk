6edcb5cd494bd6a6888bc2a8938e5973ae352fe5
Isaac Chen <tingyi364@gmail.com>
2021-08-27 15:12:05 +0800

    OP4A79: sepolicy: Initial sepolicy rules
    Signed-off-by: Isaac Chen <tingyi364@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -109,7 +109,6 @@ GNSS_HIDL_VERSION := 2.0
 
 # Kernel
 BOARD_KERNEL_CMDLINE := androidboot.hardware=qcom androidboot.memcg=1 lpm_levels.sleep_disabled=1 video=vfb:640x400,bpp=32,memsize=3072000 msm_rtb.filter=0x237 service_locator.enable=1 androidboot.usbcontroller=a600000.dwc3 swiotlb=2048 loop.max_part=7 cgroup.memory=nokmem,nosocket
-BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
 BOARD_KERNEL_IMAGE_NAME := Image
 BOARD_BOOTIMG_HEADER_VERSION := 2
 BOARD_KERNEL_BASE := 0x00000000
