40c3d965cc6101d623b5724f5f5f29defee5243e
Alexander Koskovich <akoskovich@pm.me>
2022-01-30 17:29:34 -0700

    bitra: Import vendor framework compatibiblity matrix.
     * From LA.QSSI.12.0.r1-05800.02-qssi.0, this should
       be compatible with nearly every single QCOM device
       currently supported.
    
    Change-Id: I794b894792cb5be6a1a7852dfd36d77751d76e23

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -107,7 +107,7 @@ TARGET_SURFACEFLINGER_UDFPS_LIB := //hardware/oplus:libudfps_extension.oplus
 # HIDL
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
     $(DEVICE_PATH)/device_framework_matrix.xml \
-    hardware/qcom-caf/common/vendor_framework_compatibility_matrix.xml
+    $(DEVICE_PATH)/vendor_framework_compatibility_matrix.xml
 DEVICE_MATRIX_FILE := $(DEVICE_PATH)/compatibility_matrix.xml
 DEVICE_MANIFEST_FILE := $(DEVICE_PATH)/manifest.xml
 
