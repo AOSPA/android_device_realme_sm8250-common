526aa4f66544ca7777059bb8e523cf532cab9e2b
Bruno Martins <bgcngm@gmail.com>
2022-04-12 12:34:23 +0100

    bitra: Drop QTI WiFi interfaces

--- device.mk
+++ device.mk
@@ -428,8 +428,6 @@ PRODUCT_PACKAGES += \
     libwifi-hal-ctrl \
     libwifi-hal-qcom \
     libwpa_client \
-    vendor.qti.hardware.wifi.hostapd@1.2.vendor \
-    vendor.qti.hardware.wifi.supplicant@2.2.vendor \
     WifiOverlay \
     wpa_supplicant \
     wpa_supplicant.conf
