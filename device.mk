3ff7cbf8dab9fb1a774a9729d8577aef028a7897
TheMalachite <eliasgheeraert@gmail.com>
2022-05-22 16:00:40 +0200

    bitra: Drop support for vendor.lineage.hardware.touch@1.0-service.oplus

--- device.mk
+++ device.mk
@@ -438,10 +438,6 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.telephony.ims.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.telephony.ims.xml \
     frameworks/native/data/etc/android.software.sip.voip.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.sip.voip.xml
 
-# Touch
-PRODUCT_PACKAGES += \
-    vendor.lineage.touch@1.0-service.oplus
-
 # USB
 PRODUCT_PACKAGES += \
     android.hardware.usb@1.3-service-qti
