9c682aa4bda20cdc62e1f82379d294c8afdd03c4
TheMalachite <eliasgheeraert@gmail.com>
2022-06-28 13:59:16 +0200

    bitra: Cleanup properties

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -174,7 +174,6 @@ TARGET_TAP_TO_WAKE_NODE := "/proc/touchpanel/double_tap_enable"
 TARGET_ODM_PROP += $(DEVICE_PATH)/odm.prop
 TARGET_PRODUCT_PROP += $(DEVICE_PATH)/product.prop
 TARGET_SYSTEM_EXT_PROP += $(DEVICE_PATH)/system_ext.prop
-TARGET_SYSTEM_PROP += $(DEVICE_PATH)/system.prop
 TARGET_VENDOR_PROP += $(DEVICE_PATH)/vendor.prop
 
 # QCOM
