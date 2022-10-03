ccf99a0ca4c5d6608e5df5c146413da7b8a7adf9
TheMalachite <eliasgheeraert@gmail.com>
2022-10-03 23:51:52 +0200

    sm8250-common: Rework ssss and dsds init property logic
    The current script doesn't seems to really work, it's better to set specifically which variant need the ssss or dsds property

--- init/init.oplus.sh
+++ init/init.oplus.sh
@@ -5,6 +5,10 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-if grep -q simcardnum.doublesim=1 /proc/cmdline; then
+prjname=`getprop ro.boot.prjname`
+
+if [ "$prjname" = "19706" ] || [ "$prjname" = "19795" ] || [ "$prjname" = "21619" ] || [ "$prjname" = "2161A" ] || [ "$prjname" = "2169A" ] || [ "$prjname" = "2169B" ]; then
     setprop vendor.radio.multisim.config dsds
+elif [ "$prjname" = "19705" ]; then
+    setprop vendor.radio.multisim.config ssss
 fi
