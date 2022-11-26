873e7b1b39c86fb8bdc7917210b0b445a6eb50ef
Mashopy <eliasgheeraert@gmail.com>
2022-11-26 23:34:06 +0100

    sm8250-common: Stop building Oplus light service

--- common.mk
+++ common.mk
@@ -129,10 +129,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.keymaster@4.1.vendor
 
-# Light
-PRODUCT_PACKAGES += \
-    android.hardware.light-service.oplus
-
 # Net
 PRODUCT_PACKAGES += \
     netutils-wrapper-1.0
