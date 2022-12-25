d50ac0d76951e89996ed8f0844791b07a73d1a84
Mashopy <eliasgheeraert@gmail.com>
2022-12-25 07:21:22 +0100

    sm8250-common: Drop Camera2 from target list
    Google CameraGO is more user friendly

--- common.mk
+++ common.mk
@@ -49,9 +49,6 @@ PRODUCT_PACKAGES += \
     libcamera2ndk_vendor \
     libstdc++.vendor
 
-PRODUCT_PACKAGES += \
-    Camera2
-
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.camera.flash-autofocus.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.flash-autofocus.xml \
     frameworks/native/data/etc/android.hardware.camera.front.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.front.xml \
