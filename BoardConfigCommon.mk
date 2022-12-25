02b8764b01ed5c090f322cafd0cf440a00f13f1a
Mashopy <eliasgheeraert@gmail.com>
2022-12-25 07:23:13 +0100

    sm8250-common: Remove Qualcomm compatiblity matrix from our BoardConfig
    Already included by device/qcom/common/common.mk

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -55,9 +55,7 @@ TARGET_FS_CONFIG_GEN := $(COMMON_PATH)/config.fs
 TARGET_SURFACEFLINGER_UDFPS_LIB := //hardware/oplus:libudfps_extension.oplus
 
 # HIDL
-DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
-    $(COMMON_PATH)/device_framework_matrix.xml \
-    vendor/qcom/opensource/core-utils/vendor_framework_compatibility_matrix.xml
+DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += $(COMMON_PATH)/device_framework_matrix.xml
 DEVICE_MATRIX_FILE := $(COMMON_PATH)/compatibility_matrix.xml
 DEVICE_MANIFEST_FILE := $(COMMON_PATH)/manifest.xml
 
