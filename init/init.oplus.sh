0ee295dc99ee24e2721817c80ca72ea00e6ab067
Mashopy <eliasgheeraert@gmail.com>
2023-02-10 19:22:18 +0100

    sm8250-common: init: Decommonize libnfc-nxp configs for some projects
    Change-Id: I2150ae4e2bbeca3f4fe0d6b892686cda031a526a
    Signed-off-by: Mashopy <eliasgheeraert@gmail.com>

--- init/init.oplus.sh
+++ init/init.oplus.sh
@@ -7,8 +7,20 @@
 
 prjname=`getprop ro.boot.prjname`
 
+# Radio
 if [ "$prjname" = "19706" ] || [ "$prjname" = "19795" ] || [ "$prjname" = "21619" ] || [ "$prjname" = "2161A" ] || [ "$prjname" = "2169A" ] || [ "$prjname" = "2169B" ]; then
     setprop vendor.radio.multisim.config dsds
 elif [ "$prjname" = "19705" ]; then
     setprop vendor.radio.multisim.config ssss
+fi
+
+# NFC
+if [ "$prjname" = "19705" ] || [ "$prjname" = "19706" ]; then
+    setprop persist.vendor.nfc.config_file_name "libnfc-nxp-19705.conf"
+elif [ "$prjname" = "19795" ]; then
+    setprop persist.vendor.nfc.config_file_name "libnfc-nxp-19795.conf"
+elif [ "$prjname" = "21619" ] || [ "$prjname" = "2161A" ]; then
+    setprop persist.vendor.nfc.config_file_name "libnfc-nxp-21619.conf"
+elif [ "$prjname" = "2169A" ] || [ "$prjname" = "2169B" ]; then
+    setprop persist.vendor.nfc.config_file_name "libnfc-nxp-2169B.conf"
 fi
