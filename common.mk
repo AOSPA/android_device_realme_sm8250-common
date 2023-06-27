3a7b6f54b84cad81910ace9e6791d36eb0840f71
Mashopy <eliasgheeraert@gmail.com>
2023-06-27 21:56:44 +0200

    [SQUASH] sm8250-common: Revert back to FCM level 5
    This reverts commit 5094c9c7118be5a3b545c61f01d1fa9eef2473f8.
    
    Change-Id: I12c0c1457b985c27f0ad3fedaac4752d67becaeb

--- common.mk
+++ common.mk
@@ -61,8 +61,6 @@ PRODUCT_COPY_FILES += \
 PRODUCT_CHARACTERISTICS := nosdcard
 
 # Display
-TARGET_DISABLE_MEMTRACK := true
-
 PRODUCT_PACKAGES += \
     android.hardware.graphics.common-V1-ndk.vendor
 
