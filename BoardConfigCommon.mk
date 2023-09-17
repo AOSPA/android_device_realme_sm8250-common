1c5bcd66c365762c4049d582aa85e6e608c4a7d4
Mashopy <eliasgheeraert@gmail.com>
2023-09-17 20:21:48 +0200

    sm8250-common: Add support for OZO Audio
    Change-Id: I2208a54eae80dc89519c52569996cc948e1c863e

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -58,7 +58,9 @@ TARGET_SURFACEFLINGER_UDFPS_LIB := //hardware/oplus:libudfps_extension.oplus
 # HIDL
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += $(COMMON_PATH)/device_framework_matrix.xml
 DEVICE_MATRIX_FILE := $(COMMON_PATH)/compatibility_matrix.xml
-DEVICE_MANIFEST_FILE := $(COMMON_PATH)/manifest.xml
+DEVICE_MANIFEST_FILE := \
+    $(COMMON_PATH)/manifest.xml \
+    $(COMMON_PATH)/configs/vintf/c2_manifest_vendor.xml
 
 # Init
 TARGET_INIT_VENDOR_LIB := //$(COMMON_PATH):init_realme_kona
