887dc4c1bc182391fc6398f7a5f73a6cb7340eeb
Mashopy <eliasgheeraert@gmail.com>
2023-11-14 12:15:56 +0100

    [DNM] sm8250-common: Temporary disable ozoaudio C2 service
    Android 14 introduced a new missing symbol [1] on libcodec2_vndk.so due to [2]
    This is a temporary fix for audio and camera (ozoaudio break these when the service crash) until that symbol get shimed
    
    [1]: F linker  : CANNOT LINK EXECUTABLE "/odm/bin/hw/vendor.ozoaudio.media.c2@1.0-service": cannot locate symbol "_ZN17C2PooledBlockPoolC1ERKNSt3__110shared_ptrI11C2AllocatorEEy" referenced by "/odm/lib/libozoc2store.so"
    [2]: https://android.googlesource.com/platform/frameworks/av/+/be2fbc2d7067aa4309d9f49a117e0b1cece7702c
    
    Change-Id: I557591b20b85ea65a5b66e7a240286131cc2428b

--- extract-files.sh
+++ extract-files.sh
@@ -63,6 +63,11 @@ function blob_fixup() {
         odm/bin/hw/vendor.ozoaudio.media.c2@1.0-service|odm/lib/libcodec2_soft_ozodec.so|odm/lib/libcodec2_soft_ozoenc.so)
             "${PATCHELF}" --add-needed "libshims_ozoc2store.so" "${2}"
             ;;
+        odm/etc/init/vendor.ozoaudio.media.c2@1.0-service.rc)
+            cat << EOF >> "${2}"
+    disabled
+EOF
+            ;;
         system_ext/etc/permissions/com.android.hotwordenrollment.common.util.xml)
             sed -i "s/my_product/system_ext/" "${2}"
             ;;
